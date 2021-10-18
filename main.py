164acfbfe15785f850d14b4bcf4701bd32496920
Király Tamás <tomaszpest@gmail.com>
2021-10-18 20:49:26 +0200

    Elkezdtem a Python tanulást.

---
+++ main.py
@@ -0,0 +1,10 @@
+print('Szép cica, jó cica,\nSelymes a bundád.\nKedves cica, álmos cica,\nBújj hozzám.')
+print("A ház nem hivatalos, ám közismert jelmondata: 'Egy Lannister mindig megfizeti az adósságait.'\nA valódi jelmondatuk ezzel szemben: 'Halld üvöltésem!'")
+print(str(101)+" kiskutya")
+print('"Ne akarj másnak látszani, mint aminek látszol, mert ha nem annak akarsz látszani, aminek látszol, akkor nem annak látszol, aminek látszani akarnál." – Alice Tükörországban' ==
+      '"Ne akarj másnak látszani, mint aminek látszol. Mert ha nem annak akarsz látszani, aminek látszol, akkor nem annak látszol, aminek látszani akarnál." – Alice Tükörországban')
+print('Batman and RObin' != 'Barney and Robin')
+gas_prize = 384
+print(600*6.5/100*gas_prize)
+ultimate_answer = 1890/90*2
+print('A válasz az életre, a világmindenségre, meg mindenre: ' + str(ultimate_answer))
