edd7202cc0b4a619c31f12e2ea3c0bcd214027ae
Király Tamás <tomaszpest@gmail.com>
2021-10-20 20:35:43 +0200

    kész

---
+++ strings.py
@@ -0,0 +1,6 @@
+string="keret"
+print(string[4]+string[3]+string[2]+string[1]+string[0])
+print(string[2]+string[1]+string[4]+string[3]+string[0])
+print(string[4]+string[1]+string[0]+string[3]+string[2])
+longest_hungarian_word='megszentségteleníthetetlenségeskedéseitekért'
+print(longest_hungarian_word[-1])
