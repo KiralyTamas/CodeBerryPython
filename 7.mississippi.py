00a5b632942628b5d6c32a34ccf777b0a913b654
KiralyTamas <tomaszpest@gmail.com>
2021-10-21 12:34:53 +0200

    Kiszámolgatva

---
+++ 7.mississippi.py
@@ -0,0 +1,7 @@
+river="mississippi"
+def check_river_letters(river):
+  print("A Mississippi szó "+str(river.count('i'))+"db \"i\" betűt tartalmaz.")
+  print("A Mississippi szó "+str(river.count('s'))+"db \"s\" betűt tartalmaz.")
+  print("A Mississippi szó "+str(river.count('p'))+"db \"p\" betűt tartalmaz.")
+
+check_river_letters(river)
