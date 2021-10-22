dd7173d73f2554544fca1dd518b04a776720e1c6
Király Tamás <tomaszpest@gmail.com>
2021-10-22 22:27:04 +0200

    Estére most ennyi elég

--- 9.bulls.py
+++ 9.bulls.py
@@ -1,4 +1,5 @@
 favorite_team=["Armstrong B.J.","Cartwright Bill","Grant Horace","Hodger Craig","Hopson Dennis","Jordan Michael","King Stacey","Levingston Cliff","Paxson John","Perdue Will","Pippen Scottie","Williams Scott"]
+los_angeles_lakers=[]
 print(favorite_team[0])
 print(favorite_team[1])
 print(favorite_team[-1])
@@ -31,6 +32,11 @@ def del_player(team):
   del team[-2:]
   print(team)
 
+def transfer_player(our_team,rival_team,player_index):
+  rival_team=our_team.pop(player_index)
+  print(our_team)
+  print(rival_team)
+
 get_middle_people(favorite_team)
 remove_injured_players(1)
 remove_injured_players(3)
@@ -39,3 +45,4 @@ add_new_player(favorite_team, "Kukoc")
 add_captain(favorite_team)
 remove_player(favorite_team, "Hopson Dennis")
 del_player(favorite_team)
+transfer_player(favorite_team,los_angeles_lakers,3)
