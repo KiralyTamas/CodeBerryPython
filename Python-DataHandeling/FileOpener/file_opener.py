2c8474b14d139f5c6cd94a3bee21bafd4d46a512
KiralyTamas <tomaszpest@gmail.com>
2021-10-28 10:32:52 +0200

    Fájlok átrendezve

--- Python-DataHandeling/FileOpener/file_opener.py
+++ Python-DataHandeling/FileOpener/file_opener.py
@@ -1,6 +1,10 @@
 def file_opener():
-  my_file = open("data1.txt","x")
-  my_file = open("data1.txt","w")
+  my_file = open("Text/data1.txt","x")
+  my_file = open("Text/data1.txt","w")
   my_file.write("Kutyafutty")
+  my_file = open("Text/data1.txt","r")
+  content=my_file.read()
+  print(content)
   my_file.close()
+
 file_opener()
