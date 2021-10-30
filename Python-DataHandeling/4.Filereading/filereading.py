1e4fddc4bf299f8237fb326d32a309077590242b
Király Tamás <tomaszpest@gmail.com>
2021-10-30 13:22:10 +0200

    Kész

--- Python-DataHandeling/4.Filereading/filereading.py
+++ Python-DataHandeling/4.Filereading/filereading.py
@@ -1,13 +1,19 @@
 def file_reading(file_name):
   try:
-    file=open(file_name,"r")
-    content=file.read()
-    print(content)
-    file.close()
-  except FileNotFoundError():
-    print("Ilyen fájl nem található")
+    try:
+      file=open(file_name,"r")
+    except NameError:
+      print("Valami nem jó")
+    else:
+      content=file.read()
+      print(content)
+      file.close()
+  except OSError:
+    print("Hiba a fájl kezelése közben!")
+  finally:
+    print("Kérdezés Vége")
 
 def get_file_name():
-    return input("Mi a fájl elérési útvonala?" )
+    return input("Mi a fájl elérési útvonala? ")
 
 file_reading(get_file_name())
