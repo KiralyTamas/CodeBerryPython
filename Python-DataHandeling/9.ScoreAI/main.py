c549320fcde5856bae9d8b30440027fd1b7b421c
Király Tamás <tomaszpest@gmail.com>
2021-11-02 22:59:15 +0100

    Na ez már most kezd alakúlni :)

--- Python-DataHandeling/9.ScoreAI/main.py
+++ Python-DataHandeling/9.ScoreAI/main.py
@@ -3,14 +3,20 @@ import csv
 
 old="json"
 new="csv"
-
+data_1=[]
 path=input("Mi a konvertálandó fájl útvonala: ")
 source=open(path,"r")
 data=json.load(source)
 source.close()
+data_1=data[0]
 csv_path=path.replace(old, new)
 csv_file=csv.writer(open(csv_path,"w"))
+for i in data_1:
+  if isinstance(data_1[i], dict):
+    for j in data_1[i]:
+      print(data_1[i][j])
 
-print(data)
-print(path)
-print(csv_path)
+csv_file.writerow(data_1.keys())
+csv_file.writerow(data_1.values())
+print(data_1)
+print(data_1.keys())
