4dcdda31cb92b1f4efe66846488f72779e6205f2
KiralyTamas <tomaszpest@gmail.com>
2021-11-03 15:55:18 +0100

    Otthon folytatni kell és megcsinálni :)

--- Python-DataHandeling/9.ScoreAI/main.py
+++ Python-DataHandeling/9.ScoreAI/main.py
@@ -3,20 +3,32 @@ import csv
 
 old="json"
 new="csv"
-data_1=[]
+data=[]
 path=input("Mi a konvertálandó fájl útvonala: ")
 source=open(path,"r")
 data=json.load(source)
 source.close()
-data_1=data[0]
 csv_path=path.replace(old, new)
 csv_file=csv.writer(open(csv_path,"w"))
-for i in data_1:
-  if isinstance(data_1[i], dict):
-    for j in data_1[i]:
-      print(data_1[i][j])
 
-csv_file.writerow(data_1.keys())
-csv_file.writerow(data_1.values())
-print(data_1)
-print(data_1.keys())
+def convert_header(data,delim):
+  header=[]
+  content=[]
+  for number in data:
+    for i in number.keys():
+      if isinstance(number[i], dict):
+        for j in number[i].keys():
+          header.append(str(i)+delim+str(j))
+      header.append(i)
+      if isinstance(number[i], dict):
+        header.remove(i)
+    for i in number.keys():
+      if isinstance(number[i], dict):
+        for j in number[i]:
+          content.append(number[i][j])
+      content.append(number[i])
+
+  csv_file.writerow(header)
+  csv_file.writerow(content)
+
+convert_header(data, "_")
