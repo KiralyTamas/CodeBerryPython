63b9f043d78e8b81e7c75a172e5783ad60fcebdf
Király Tamás <tomaszpest@gmail.com>
2021-11-04 20:36:44 +0100

    Költözés készül.

--- Python-DataHandeling/9.ScoreAI/match/main.py
+++ Python-DataHandeling/9.ScoreAI/match/main.py
@@ -4,7 +4,7 @@ import json
 old = "json"
 new = "csv"
 data = []
-path="D:\Repository\CodeBerryPython\Python-DataHandeling\9.ScoreAI\match\match16000.json"
+path="match16000.json"
 #path=input("Mi a konvertálandó fájl útvonala: ")
 source = open(path, "r",encoding="utf-8")
 data = json.load(source)
